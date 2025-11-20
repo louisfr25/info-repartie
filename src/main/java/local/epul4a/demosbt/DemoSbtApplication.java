6d3cc95976673fc598187360fbacccd8139d9343
Arthur Joly <a.joly3821@gmail.com>
2025-11-20 10:10:28 +0100

    add applciation

---
+++ src/main/java/local/epul4a/demosbt/DemoSbtApplication.java
@@ -0,0 +1,13 @@
+package local.epul4a.demosbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSbtApplication.class, args);
+    }
+
+}
